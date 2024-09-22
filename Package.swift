88edecf6d02fbb9f730a781b841c90b8f31babff
Loïc Salanon <loic@salanon.fr>
2024-09-22 17:35:57 +0200

    correction supported platform

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "HyperText",
     platforms: [
-        .iOS(.v18)
+        .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
